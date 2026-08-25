ce62c5341d5b57e79ed5ce8ef54caaa618baf382
CMF66 <3218415602@qq.com>
2026-08-25 16:05:57 +0800

    Update warm_up_multi_step_lr.py

--- scheduler/warm_up_multi_step_lr.py
+++ scheduler/warm_up_multi_step_lr.py
@@ -1,15 +1,6 @@
-# encoding: utf-8
-"""
-@author:  liaoxingyu
-@contact: sherlockliao01@gmail.com
-"""
+
 from bisect import bisect_right
 import torch
-
-
-# FIXME ideally this would be achieved with a CombinedLRScheduler,
-# separating MultiStepLR with WarmupLR
-# but the current LRScheduler design doesn't allow it
 
 class WarmupMultiStepLR(torch.optim.lr_scheduler._LRScheduler):
     def __init__(
