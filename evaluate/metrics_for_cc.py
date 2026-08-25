7f9f48ac95b1469565a61dd00ec35a24505119a2
CMF66 <3218415602@qq.com>
2026-08-25 16:18:28 +0800

    Update metrics_for_cc.py

--- evaluate/metrics_for_cc.py
+++ evaluate/metrics_for_cc.py
@@ -1,12 +1,3 @@
-"""
-@author:  Qizao Wang
-@contact: qzwang22@m.fudan.edu.cn
-
-TIFS 2024 paper: Exploring Fine-Grained Representation and Recomposition for Cloth-Changing Person Re-Identification
-URL: https://ieeexplore.ieee.org/document/10557733
-GitHub: https://github.com/QizaoWang/FIRe-CCReID
-"""
-
 import numpy as np
 from tqdm import trange
 
@@ -22,30 +13,29 @@ def evaluate_ltcc(distmat, q_pids, g_pids, q_camids, g_camids, q_clothids, g_clo
     indices = np.argsort(distmat, axis=1)
     matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
 
-    # compute cmc curve for each query
+ 
     all_cmc = []
     all_AP = []
-    num_valid_q = 0. # number of valid query
+    num_valid_q = 0. 
 
     for q_idx in trange(num_q):
-        # get query pid and camid
         q_pid = q_pids[q_idx]
         q_camid = q_camids[q_idx]
         q_clothid = q_clothids[q_idx]
 
         order = indices[q_idx]
-        if ltcc_cc_setting:  # remove gallery samples that have the same pid and (camid or clothid) with query
+        if ltcc_cc_setting: 
             remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid) | \
                      (g_pids[order] == q_pid) & (g_clothids[order] == q_clothid)
-        else:  # remove gallery samples that have the same pid and camid with query
+        else:  
             remove = (g_pids[order] == q_pid) & (g_camids[order] == q_camid)
         keep = np.invert(remove)
 
-        # compute cmc curve
+    
         raw_cmc = matches[q_idx][
-            keep]  # binary vector, positions with value 1 are correct matches
+            keep] 
         if not np.any(raw_cmc):
-            # this condition is true when query identity does not appear in gallery
+          
             continue
 
         cmc = raw_cmc.cumsum()
@@ -54,8 +44,6 @@ def evaluate_ltcc(distmat, q_pids, g_pids, q_camids, g_camids, q_clothids, g_clo
         all_cmc.append(cmc[:max_rank])
         num_valid_q += 1.
 
-        # compute average precision
-        # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
         num_rel = raw_cmc.sum()
         tmp_cmc = raw_cmc.cumsum()
         tmp_cmc = [x / (i+1.) for i, x in enumerate(tmp_cmc)]
@@ -82,10 +70,9 @@ def evaluate_prcc_all_gallery(distmat, q_pids, g_pids, max_rank=50):
     indices = np.argsort(distmat, axis=1)
     matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
 
-    # compute cmc curve for each query
     all_cmc = []
     all_AP = []
-    num_valid_q = 0. # number of valid query
+    num_valid_q = 0. 
 
     for q_idx in trange(num_q):
         raw_cmc = matches[q_idx]
@@ -95,8 +82,8 @@ def evaluate_prcc_all_gallery(distmat, q_pids, g_pids, max_rank=50):
         all_cmc.append(cmc[:max_rank])
         num_valid_q += 1.
 
-        # compute average precision
-        # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
+        
+       
         num_rel = raw_cmc.sum()
         tmp_cmc = raw_cmc.cumsum()
         tmp_cmc = [x / (i+1.) for i, x in enumerate(tmp_cmc)]
